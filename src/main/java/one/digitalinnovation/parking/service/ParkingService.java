528e2608706dcfcb0bbae5c955e65a12c610f76d
viniciusbdzn <viniciusbdzn@gmail.com>
2022-11-26 19:49:31 -0300

    first endpoint

---
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.parking.service;
+
+import one.digitalinnovation.parking.model.Parking;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class ParkingService {
+
+    private static Map<String, Parking> parkingMap = new HashMap();
+
+    static {
+        var id = getUUID();
+        Parking parking = new Parking(id, "USD-6349", "RJ", "DUSTER", "PRETO");
+        parkingMap.put(id, parking);
+    }
+
+    public List<Parking> findAll() {
+        return parkingMap.values().stream().collect(Collectors.toList());
+    }
+    private static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+}
